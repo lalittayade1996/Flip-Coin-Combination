94de84aa02141e0f3da5f5f5f093467b1c954971
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 21:37:15 +0530

    Added: UC3-Third-Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
+declare -A flip
+random(){
 if [ $(( RANDOM%2 )) -eq 0 ]
 then
 echo H
 else
 echo T
 fi
+}
+for(( i=0;i<5;i++ ))
+do
+f=$( random )
+flip[$i]=$f
+winper=$(( 100/2 ))
+done
+for(( i=5;i<10;i++ ))
+do
+r=$(random)$(random)
+flip[$i]=$r
+winper2=$(( 100/4 ))
+done
+
+echo ${flip[@]}
