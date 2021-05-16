d8baa82527cdb2efb204c2d6f6b02bb871c99751
lalittayade1996 <lalittayade2.9.1996@gmail.com>
2021-05-16 17:00:39 +0530

    Added: UC2-Second-Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/bin/bash -x
 
+declare -A flip
+random(){
 if [ $(( RANDOM%2 )) -eq 0 ]
 then
 echo H
 else
 echo T
 fi
+}
+for(( i=0;i<5;i++ ))
+do
+f=$( random )
+flip[$i]=$f
+winper=$(( 100/2 ))
+done
+echo ${flip[@]}
